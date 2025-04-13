0b193d675586e9866886ff6c39616f63a16c5ad6
yehvaed <jaroslaw.walach@gmail.com>
2025-04-13 22:13:31 +0200

    feat(tmux): add catppuccin theme

--- nix-config/apps/tmux.nix
+++ nix-config/apps/tmux.nix
@@ -1,7 +1,50 @@
-{
+{ lib, ... }:
+let
+  inherit (builtins)
+    concatLists attrValues isAttr filter readDir match map exec;
+  inherit (lib.filesystem) listFilesRecursive;
+  inherit (lib.strings) removePrefix hasSuffix;
+  inherit (lib) findFirst any;
+
+  matchPattern = file:
+    any (pattern: hasSuffix pattern file) [ ".tmux" ];
+
+  findPluginFile = plugin:
+    let files = map (file: removePrefix plugin.outPath file) (listFilesRecursive plugin.outPath);
+    in findFirst matchPattern "" files;
+
+  usePlugin = plugin:
+    ''run ${plugin}${findPluginFile plugin}'';
+
+in {
   nix-config.apps.tmux = {
     home = { pkgs, ... }@inputs: {
       programs.tmux = {
+        plugins = with pkgs.tmuxPlugins; [
+          # bottom bar sections
+          cpu 
+          battery
+        ];
+
+        extraConfig = with pkgs.tmuxPlugins; ''
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+
+          ${usePlugin catppuccin}
+
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          set -agF status-right "#{E:@catppuccin_status_battery}"
+
+          ${usePlugin cpu}
+          ${usePlugin battery}
+        '';
+
         terminal = "tmux-256color";
         historyLimit = 100000;
 

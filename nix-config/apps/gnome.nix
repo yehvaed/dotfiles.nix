727f74c85bab7ce20839ecf961b4728fa9f48c7e
yehvaed <jaroslaw.walach@gmail.com>
2025-04-15 23:01:20 +0200

    feat(kde): add basic config

--- nix-config/apps/gnome.nix
+++ nix-config/apps/gnome.nix
@@ -1,11 +1,9 @@
 {
   nix-config.apps.gnome = {
     nixos = { pkgs, ... }: {
-      services.xserver = {
-        enable = true;
-        displayManager.gdm.enable = true;
-        desktopManager.gnome.enable = true;
-      };
+      # services.xserver.enable = true;
+      # services.displayManager.sddm.enable = true;
+      # services.desktopManager.plasma6.enable = true;
     };
 
     tags = [ "gui" ];
---
+++ nix-config/apps/kde.nix
@@ -0,0 +1,15 @@
+{
+  nix-config.apps.gnome = {
+    nixos = { pkgs, ... }: {
+      services.xserver.enable = true;
+      services.displayManager.sddm.enable = true;
+      services.desktopManager.plasma6.enable = true;
+
+      environment.systemPackages = with pkgs; [
+          catppuccin-kde
+      ];
+    };
+
+    tags = [ "gui" ];
+  };
+}

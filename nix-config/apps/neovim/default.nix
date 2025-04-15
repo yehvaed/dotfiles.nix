92e805cb3066a6d176af8896c2b5ef2033aa7814
yehvaed <jaroslaw.walach@gmail.com>
2025-04-15 23:55:34 +0200

    feat(neovim): add basic config

---
+++ nix-config/apps/neovim/default.nix
@@ -0,0 +1,53 @@
+{
+  nix-config.apps.neovim = {
+    home = { pkgs, ... }@inputs: 
+    let
+      loader = import ./plugins inputs;
+    in {
+      programs.neovim = {
+        plugins = loader (with pkgs.vimPlugins; [
+          # ==> @editor/lsp
+          nvim-cmp
+          nvim-lspconfig
+          luasnip
+          cmp-nvim-lsp
+          cmp_luasnip
+
+          # ==> @editor/treesitter
+          nvim-treesitter.withAllGrammars
+
+          # ==> @editor
+          neo-tree-nvim
+          trouble-nvim
+
+          # ==> @pickers
+          fzf-lua
+
+          # ==> @theme
+          neovim-ayu
+
+          # ==> @tools
+          neorg
+        ]);
+
+        extraConfig = ''
+          let mapleader = " "
+          set tabstop=2 softtabstop=2
+          set shiftwidth=2
+          set expandtab
+          set smartindent
+          set number
+        '';
+
+        viAlias = true;
+        vimAlias = true;
+
+        enable = true;
+      };
+    };
+
+    nixos = { environment.variables = { EDITOR = "nvim"; }; };
+
+    tags = [ "dev" ];
+  };
+}

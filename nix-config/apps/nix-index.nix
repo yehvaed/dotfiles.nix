7577c1804b38eb556d67bee2d0510cb245c1a9f0
yehvaed <jaroslaw.walach@gmail.com>
2025-04-20 23:47:08 +0200

    feat(nix-index): add nix-locate and comma

---
+++ nix-config/apps/nix-index.nix
@@ -0,0 +1,13 @@
+{ inputs, ...}: {
+  nix-config.apps.nix-index = {
+    nixos = { pkgs, ... }: {
+      programs.nix-index-database.comma.enable = true;
+    };
+
+    tags = [ "default" ];
+  };
+
+  nix-config = {
+    modules.nixos = [ inputs.nix-index-database.nixosModules.nix-index ];
+  };
+}
